be3b5e10912188d6d8ad2d23a93e16bf40ea8191
Suresh Vankadara <svankada@codeaurora.org>
2013-05-02 17:12:08 +0530

    msm: camera: Fix camera crash when ctrl_q is empty
    Add proper error condition if the list is empty and
    return error value to upper layers instead of BUG_ON.
    
    Change-Id: I89237e7a27d5fb1e1fd5f1a8a2dfd7ea9a1afff5
    CRs-Fixed: 481759
    Signed-off-by: Suresh Vankadara <svankada@codeaurora.org>

--- drivers/media/video/msm/server/msm_cam_server.c
+++ drivers/media/video/msm/server/msm_cam_server.c
@@ -570,11 +570,17 @@ static int msm_server_control(struct msm_cam_server_dev *server_dev,
 #endif
 /* LGE_CHANGE_E, camera recovery patch, 2013.1.16, jungki.kim[End] */
 			return rc;
+		} else {
+			pr_err("%s: List is empty\n", __func__);
+			return -EINVAL;
 		}
 	}
 
 	rcmd = msm_dequeue(queue, list_control);
-	BUG_ON(!rcmd);
+	if (!rcmd) {
+		pr_err("%s: List is empty\n", __func__);
+		return -EINVAL;
+	}
 	D("%s Finished servicing ioctl\n", __func__);
 
 	ctrlcmd = (struct msm_ctrl_cmd *)(rcmd->command);
