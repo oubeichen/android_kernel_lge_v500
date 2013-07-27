41d784776f0dafd23665d4d46ab584e3f8836c81
Dennis Rassmann <showp1984@gmail.com>
2013-07-27 23:33:17 +0200

    cpuinit: get rid of __cpuinit, first regexp This commit is the result of:
    find -name "*.c" | xargs sed -i "s/ __cpuinit / /g"
    
    Signed-off-by: Arjan van de Ven <arjan@linux.intel.com>
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>
    
    Conflicts:
    	arch/arm/mach-msm/acpuclock-krait.c
    
    Conflicts:
    	arch/arm/kernel/arch_timer.c
    	arch/arm/kernel/smp.c
    	arch/arm/mach-msm/acpuclock-krait.c
    	kernel/workqueue.c
    
    Conflicts:
    	arch/arm/mach-msm/cpufreq.c

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -325,7 +325,7 @@ unsigned int get_cur_max(unsigned int cpu)
         return limit->allowed_max;
 }
 
-static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
+static int msm_cpufreq_init(struct cpufreq_policy *policy)
 {
 	int cur_freq;
 	int index;
@@ -389,7 +389,7 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 	return 0;
 }
 
-static int __cpuinit msm_cpufreq_cpu_callback(struct notifier_block *nfb,
+static int msm_cpufreq_cpu_callback(struct notifier_block *nfb,
 		unsigned long action, void *hcpu)
 {
 	unsigned int cpu = (unsigned long)hcpu;
