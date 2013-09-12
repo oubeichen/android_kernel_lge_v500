70cf67f6037318264d8f5b9eb3e2660e461c3127
Iliyan Malchev <malchev@google.com>
2013-09-12 14:45:16 -0700

    prima: release v3.2.3.5
    	git://codeaurora.org/external/wlan/prima.git
    
    	b1d7f6b wlan : Revision 3.2.3.5
    	645ec74 wlan: Fix for "crash in pMac->cfg.gCfgEntry"
    	2e245b2 wlan: wlan: Change regulatory followed customer request
    	8e953f1 wlan: Fix the issue with probe req data rate on 5G channels.
    	1ce9188 wlan : Revision 3.2.3.4
    	5afb9c9 wlan:Increase CSR_RSN_MAX_AUTH_SUITES to 16
    	4be8f6a wlan : Revision 3.2.3.3
    	c3fc2f9 wlan: Update value of WLAN_HAL_PNO_MAX_NETW_CHANNELS
    	2cdd9a2 wlan: Fix invalid channel issue for p2p due to 11d change
    	c44b861 Fix for making TDLS max peer count independent of IBSS
    	d745d66 wlan: Fix for the assert in hdd_rx_packet_cbk
    	dcc4602 Wlan: Fix for device not entering into BMPS mode
    	29796b4 wlan: remove duplicate IF condition variable checks
    	a1f061f wlan: Fix for spectrum management bit in AddBss for SM disabled AP
    	972c1c0 wlan: DTIM3 changed to DTIM1 after roaming
    	6a4d0ed Wlan: Enable/Disable dynamic split scan
    	aa35216 wlan: Add more debug log for NS offload feature
    	35938f3 wlan: restore filter setting on resume
    	ad2ed83 wlan: Enable WLAN_NS_OFFLOAD feature flag in Kbuild
    	400721c wlan: Add NSOffload INI Param and issue NSOffload request to FW
    	eb137b7 wlan: Host Offload Cleanup
    	7229c34 wlan: MC/BC Filter cleanup
    
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- drivers/staging/prima/CORE/MAC/src/pe/lim/limAssocUtils.c
+++ drivers/staging/prima/CORE/MAC/src/pe/lim/limAssocUtils.c
@@ -3374,11 +3374,9 @@ tSirRetStatus limStaSendAddBss( tpAniSirGlobal pMac, tpSirAssocRsp pAssocRsp,
 
     if (VOS_P2P_CLIENT_MODE == psessionEntry->pePersona)
     {
-        pAddBssParams->staContext.p2pCapableSta = 1;       
+        pAddBssParams->staContext.p2pCapableSta = 1;
     }
-
-    pAddBssParams->bSpectrumMgtEnabled = psessionEntry->spectrumMgtEnabled || 
-        limIsconnectedOnDFSChannel(bssDescription->channelId);
+    pAddBssParams->bSpectrumMgtEnabled = psessionEntry->spectrumMgtEnabled;
 
 #if defined WLAN_FEATURE_VOWIFI_11R
     pAddBssParams->extSetStaKeyParamValid = 0;
@@ -3658,11 +3656,9 @@ tSirRetStatus limStaSendAddBssPreAssoc( tpAniSirGlobal pMac, tANI_U8 updateEntry
 
     pAddBssParams->staContext.sessionId = psessionEntry->peSessionId;
     pAddBssParams->sessionId = psessionEntry->peSessionId;
-    
-    pAddBssParams->halPersona = (tANI_U8)psessionEntry->pePersona; //update persona
 
-    pAddBssParams->bSpectrumMgtEnabled = psessionEntry->spectrumMgtEnabled || 
-        limIsconnectedOnDFSChannel(bssDescription->channelId);
+    pAddBssParams->halPersona = (tANI_U8)psessionEntry->pePersona; //update persona
+    pAddBssParams->bSpectrumMgtEnabled = psessionEntry->spectrumMgtEnabled;
 
 #if defined WLAN_FEATURE_VOWIFI_11R
     pAddBssParams->extSetStaKeyParamValid = 0;
@@ -3672,7 +3668,7 @@ tSirRetStatus limStaSendAddBssPreAssoc( tpAniSirGlobal pMac, tANI_U8 updateEntry
 
     //pMac->lim.gLimMlmState = eLIM_MLM_WT_ADD_BSS_RSP_PREASSOC_STATE;
     psessionEntry->limMlmState = eLIM_MLM_WT_ADD_BSS_RSP_PREASSOC_STATE;
-    
+
     MTRACE(macTrace(pMac, TRACE_CODE_MLM_STATE, psessionEntry->peSessionId, psessionEntry->limMlmState));
 
     //we need to defer the message until we get the response back from HAL.
