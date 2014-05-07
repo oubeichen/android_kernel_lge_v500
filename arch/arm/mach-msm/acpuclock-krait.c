05bd28bcffb91e7497ab2b92dd7cf592aa0defc2
oubeichen <oubeichen@gmail.com>
2014-05-07 09:51:11 +0800

    Revert "Revert "Low CPU Clocks: Allow ultra low CPU frequencies for better battery performance""
    This reverts commit 78a6c7ea5d3c30a361530c4d504e579624b25456.

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -44,6 +44,12 @@
 #include <mach/board_lge.h>
 #endif
 
+#ifdef CONFIG_LOW_CPUCLOCKS
+#define FREQ_TABLE_SIZE		39
+#else
+#define FREQ_TABLE_SIZE		35
+#endif
+
 /* MUX source selects. */
 #define PRI_SRC_SEL_SEC_SRC	0
 #define PRI_SRC_SEL_HFPLL	1
@@ -984,7 +990,7 @@ ssize_t acpuclk_set_vdd(char *buf)
 }
 
 #ifdef CONFIG_CPU_FREQ_MSM
-static struct cpufreq_frequency_table freq_table[NR_CPUS][35];
+static struct cpufreq_frequency_table freq_table[NR_CPUS][FREQ_TABLE_SIZE];
 
 static void __init cpufreq_table_init(void)
 {
